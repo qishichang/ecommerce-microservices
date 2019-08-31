d7b198145681536e98caf78313068c48f22030c8
qishichang <qishichang@live.com>
2019-09-01 07:44:28 +1200

    Add Product API

---
+++ ECommerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce.API.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+       [HttpGet]
+       public async Task<IEnumerable<ApiProduct>> GetAsync()
+        {
+            return new[] { new ApiProduct { Id = Guid.NewGuid(), Description = "fake" } };
+        }
+
+        [HttpPost]
+        public async Task PostAsync(ApiProduct product)
+        {
+
+        }
+    }
+}
